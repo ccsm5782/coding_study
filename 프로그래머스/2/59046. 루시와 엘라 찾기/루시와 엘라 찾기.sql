7479b324252b895019306ff66bf42414f1411d44
ccsm5782 <cccsm5782@gmail.com>
2026-09-26 20:23:51 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+SELECT
+    ANIMAL_ID,
+    NAME,
+    SEX_UPON_INTAKE
+FROM 
+    ANIMAL_INS
+WHERE
+    NAME = 'Lucy' or NAME = 'Ella' or 
+    NAME = 'Pickle' or NAME = 'Rogan' or 
+    NAME = 'Sabrina' OR NAME = 'Mitty'
+ORDER BY 
+    1
